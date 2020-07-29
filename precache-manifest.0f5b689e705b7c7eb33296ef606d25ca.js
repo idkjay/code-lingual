1855a7af01db2abc9a10d61eafefd1b6a8c9f4e0
Jay <jordanchu95@gmail.com>
2020-07-29 11:34:50 -0400

    Updates

---
+++ precache-manifest.0f5b689e705b7c7eb33296ef606d25ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838c617461f25177113c0daf5cda7643",
+    "url": "/code-lingual/index.html"
+  },
+  {
+    "revision": "ee80b39c8517412b71fa",
+    "url": "/code-lingual/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "ffc81f0963801f304172",
+    "url": "/code-lingual/static/js/2.023295d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/code-lingual/static/js/2.023295d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee80b39c8517412b71fa",
+    "url": "/code-lingual/static/js/main.12e2779d.chunk.js"
+  },
+  {
+    "revision": "0ca78f7ec02edddadcf0",
+    "url": "/code-lingual/static/js/runtime-main.769888ca.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/code-lingual/static/media/mintbean.cf18b629.png"
+  }
+]);
