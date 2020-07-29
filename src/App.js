974e5c322226f822eaa5eb5e1d368164e7e65773
Jay <jordanchu95@gmail.com>
2020-07-29 12:00:30 -0400

    setting up structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './mintbean.png';
-import './styles/index.css';
+import './App.css';
+import CodeLingual from './components/CodeLingual';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
-        <h2>HAPPY HACKING!</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">
+        <CodeLingual />
+      </header>
+      
     </div>
   );
 }
---
+++ src/components/CodeLingual.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const CodeLingual = () => {
+
+    return (
+        <div className="container">
+
+        </div>
+    )
+}
+
+export default CodeLingual;
