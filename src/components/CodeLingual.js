d91484a89c6f9ba851a7b571915dbcfa63ad7702
Jay <jordanchu95@gmail.com>
2020-07-29 13:41:33 -0400

    react pie chart

--- src/components/CodeLingual.js
+++ src/components/CodeLingual.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
+import '../styles/CodeLingual.css';
+import PieChart from './PieChart';
 
 const CodeLingual = () => {
+    const [username, setUsername] = useState("");
 
     return (
         <div className="container">
-
+            <h3>Search a GitHub user's most used programming languages.</h3>
+            <p>Based on a user's contributions to public GitHub repositories.</p>
+            <input 
+                type="text"
+                placeholder="Enter User's Github username"
+      
+            />
         </div>
     )
 }
