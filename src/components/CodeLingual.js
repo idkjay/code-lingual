bb21e423c6856187dc92f4d7698536d730baa876
Jay <jordanchu95@gmail.com>
2020-08-01 21:59:46 -0400

    fetch logic

--- src/components/CodeLingual.js
+++ src/components/CodeLingual.js
@@ -11,6 +11,65 @@ const CodeLingual = () => {
     const [followers, setFollowers] = useState(0);
     const [following, setFollowing] = useState(0);
 
+    const myHeaders = new Headers();
+    const authHeader = "Basic " + btoa(process.env.REACT_APP_GITHUB_CLIENT_ID + ":" + process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+    myHeaders.append("Authorization", authHeader);
+
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    const handleChange = (event) => {
+        setUsername(event.target.value);
+    }
+
+    const fetchUser = async () => {
+        setFetching(true);
+        const resp = await fetch(`https://api.github.com/users/${username}`, requestOptions);
+        const user = await resp.json();
+        if(user) { 
+            setUserfullname(user.name);
+            setFollowers(user.followers);
+            setFollowing(user.following);
+        } else {
+            setErrormsg("Not a valid user");
+        }
+
+        fetchUserdetails();
+        setFetching(false);
+    }
+
+    const fetchUserdetails = async () => {
+        setFetching(true);
+        const resp = await fetch(`https://api.github.com/users/${username}/repos`, requestOptions);
+        const userRepositories = await resp.json();
+        if(userRepositories && userRepositories.length > 0) {
+            const languageMap = new Map();
+            userRepositories.forEach((repo) => {
+                if(repo.language) {
+                    if(languageMap.has(repo.language)) {
+                        languageMap.set(repo.language, languageMap.get(repo.language)+1);
+                    } else {
+                        languageMap.set(repo.language, 1);
+                    }
+                }
+            });
+            setLanguages([]);
+            const l = [];
+            l.push(["Languages", "Count"]);
+            languageMap.forEach((value, key) => {
+                l.push([key, value]);
+                
+            });
+            setLanguages(languages => l);
+        } else {
+            setErrormsg("Not a valid user");
+        }
+        setFetching(false);
+    }
+
     return (
         <div className="container">
             <h3>Search a GitHub user's most used programming languages.</h3>
@@ -18,7 +77,6 @@ const CodeLingual = () => {
             <input 
                 type="text"
                 placeholder="Enter User's Github username"
-      
             />
         </div>
     )
